04b1d66832bc8647c18e873de508fd7b7921793d
renato <renatovp32@gmail.com>
2022-10-05 20:33:22 -0500

    Primer Commit

---
+++ src/main/java/com/magdalena/ProyMagdalenaServerApplication.java
@@ -0,0 +1,13 @@
+package com.magdalena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyMagdalenaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyMagdalenaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magdalena/ProyMagdalenaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdalena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyMagdalenaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
